e3e5b8715a5a4cb5c401958af1cb485561da695e
dex1cre <dex1cre@mail.ru>
2016-11-02 01:25:26 +0300

    dop word-1

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ EG = string.ascii_lowercase
 RU = ['а','б','в','г','д','е','ё','ж','з','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ь','ы','ъ','э','ю','я','и']
 
 def wr(word, transfer):
-    f = open('db1.txt', 'a')
-    f.write('\n' + word)
-    f.close()
-    f = open('db2.txt', 'a')
-    f.write('\n' + transfer)
-    f.close()
+    f1 = open('db1.txt', 'r')
+    f2 = open('db2.txt', 'r')
+    if word not in [f1, f2] and transfer not in [f1, f2]:
+        f1.close()
+        f2.close()
+        f1 = open('db1.txt', 'a')
+        f2 = open('db2.txt', 'a')
+        f1.write('\n' + word)
+        f1.close()
+        f2.write('\n' + transfer)
+        f2.close()
 
 def fd(word):
     indent1 = False
@@ -28,6 +33,9 @@ def fd(word):
         for i in f:
             j += 1
             if word in i:
+                dopWord = i
+                if "\n" in dopWord:
+                    dopWord = dopWord[:-1]
                 index = j
                 break
         f.close()
@@ -36,7 +44,7 @@ def fd(word):
         for i in f:
             j += 1
             if j == index:
-                print('Перевод' + i)
+                print(dopWord + ': ' + i)
                 break
     else:
         f = open('db2.txt', 'r')
@@ -44,6 +52,9 @@ def fd(word):
         for i in f:
             j += 1
             if word in i:
+                dopWord = i
+                if "\n" in dopWord:
+                    dopWord = dopWord[:-1]
                 index = j
                 break
         f.close()
@@ -52,7 +63,7 @@ def fd(word):
         for i in f:
             j += 1
             if j == index:
-                print('Перевод' + i)
+                print(dopWord + ': ' + i)
                 break
     
 
@@ -60,7 +71,8 @@ while True:
     x = int(input("""
 1 - добавить слово и перевод;
 2 - найти слово и перевод;
-3 - выйти:
+3 - подготовить слова для изучения;
+4 - выйти;
 """))
     if x == 1:
         word = input("Слово: ").lower()
@@ -71,6 +83,20 @@ while True:
     elif x == 2:
         word = input("Слово: ").lower()
         fd(word)
+    elif x == 3:
+        f1 = open('db1.txt','r')
+        f2 = open('db2.txt', 'r')
+        for i in f1:
+            if "\n" in i:
+                i = i[:-1]
+            for j in f2:
+                if "\n" in j:
+                    j = j[:-1]
+                if i != "" and j != "":
+                    print(i + "   :   " + j)
+                break
+        f1.close()
+        f2.close()
     else:
         input("Программа закончена, нажмите Enter")
         break
